2379f05caa200da4d62abfe970cfbf137dedf848
camster91 <33962910+camster91@users.noreply.github.com>
2021-05-10 11:51:46 -0400

    Books Update

--- jw_news_parser.py
+++ jw_news_parser.py
@@ -4,6 +4,8 @@ import pickle
 import feedparser
 import os
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
 from bs4 import BeautifulSoup
@@ -20,7 +22,8 @@ os.environ['WDM_LOG_LEVEL'] = '0'
 driver.minimize_window()
 driver.get('https://www.jw.org/en/library/videos/#en/categories/LatestVideos')
 
-time.sleep(10)
+WebDriverWait(driver,100).until(EC.presence_of_element_located(
+        (By.CLASS_NAME, "contentArea"))) 
 
 html = driver.page_source
 soup = BeautifulSoup(html, 'html.parser')
@@ -29,7 +32,7 @@ videos_list = []
 old_links = []
 
 try:
-    with open('C:/Python JW News/old_links.txt', 'rb') as filehandle:
+    with open('old_links.txt', 'rb') as filehandle:
         old_links = pickle.load(filehandle)
 except:
     print("File empty") 
@@ -49,10 +52,52 @@ for link in soup.find_all("div", {"class":"synopsis lss desc showImgOverlay hasD
         videos_list.append(links_dict)
         old_links.append(href)
 
-        with open('C:/Python JW News/old_links.txt', 'wb') as filehandle:
+        with open('old_links.txt', 'wb') as filehandle:
             pickle.dump(old_links, filehandle)
     else:
         continue
+
+
+driver.get('https://www.jw.org/en/library/books')
+
+WebDriverWait(driver,100).until(EC.presence_of_element_located(
+        (By.ID, "pubsViewResults"))) 
+
+book_list = []
+old_books = []
+pics_list = []
+
+try:
+    with open('old_books.txt', 'rb') as filehandle:
+        old_books = pickle.load(filehandle)
+except:
+    print("File empty") 
+
+html = driver.page_source
+soup = BeautifulSoup(html, 'html.parser')
+
+for link in  soup.find_all("div", {"class":"publicationDesc"}):
+    href = "https://www.jw.org" + link.a['href']
+    title = link.text.strip()
+
+    books_dict = {}
+    books_dict['Link'] = href
+    books_dict['Title'] = title
+
+    if href not in old_books:
+
+        book_list.append(books_dict)
+        old_books.append(href)
+
+        with open('old_books.txt', 'wb') as filehandle:
+                pickle.dump(old_books, filehandle)
+    else:
+        continue
+
+for pic in soup.find_all("div", {"class":"cvr-wrapper"}):
+    image = pic.img['src']
+
+    pics_list.append(image)
 
 d = feedparser.parse('https://www.jw.org/en/whats-new/rss/WhatsNewWebArticles/feed.xml')
 
@@ -60,7 +105,7 @@ news_list = []
 old_news = []
 
 try:
-    with open('C:/Python JW News/old_news.txt', 'rb') as filehandle:
+    with open('old_news.txt', 'rb') as filehandle:
         old_news = pickle.load(filehandle)
 except:
     print("File empty")
@@ -84,17 +129,18 @@ for entry in d.entries:
         news_list.append(news_dict)
         old_news.append(news_link)
 
-        with open('C:/Python JW News/old_news.txt', 'wb') as filehandle:
+        with open('old_news.txt', 'wb') as filehandle:
             pickle.dump(old_news, filehandle)
     else:
         continue
-    
+
 videos = ""
 news = ""
+books = ""
 count = 0
 
 for i in videos_list:
-    videos += open("C:/Python JW News/email.html").read().format(Text1=videos_list[count]['Title'], 
+    videos += open("email.html").read().format(Text1=videos_list[count]['Title'], 
                                             Link1=videos_list[count]['Link'], 
                                             Img1=videos_list[count]['Image'])
     count += 1
@@ -103,27 +149,40 @@ count = 0
 
 if videos_list != []:
     videos = "<h3>Latest Videos</h3>" + videos
-    call(["python", "D:\JW.ORG\\New\JWAutoDLNew.py"])
+    #call(["python", "D:\JW.ORG\\New\JWAutoDLNew.py"])
+
 
 for i in news_list:
-    news += open("C:/Python JW News/news.html").read().format(Text2=news_list[count]['Title'], 
+    news += open("news.html").read().format(Text2=news_list[count]['Title'], 
                                             Link2=news_list[count]['Link'], 
                                             Img2=news_list[count]['Image'])
     count += 1
 
+count = 0
+
 if news_list != []:
     news = "<h3>Latest News</h3>" + news
+
+for i in book_list:
+    books += open("books.html").read().format(Text3=book_list[count]['Title'], 
+                                            Link3=book_list[count]['Link'], 
+                                            Img3=pics_list[count])
+    count += 1
+
+if book_list != []:
+    books = "<h3>Latest Books</h3>" + books
     
 me = "JW Newsfeed"
-you = "jworgnewsfeed@gmail.com"  
-them = ["biancabashley@gmail.com","camster91@gmail.com",
-        "kesjasamotik@hotmail.com","matthewplawn@gmail.com",
-        "E.obszanska@hotmail.com","aaronm631@gmail.com",
-        "nancyjashley@live.com","gordlashley@gmail.com",
-        "Daniel-d-janeiro@live.ca", "chrislawrence4@hotmail.com",
-        "mariah_papiah@hotmail.com", "aaroncastillo86@gmail.com",
-        "janitacastillo85@gmail.com"
-       ]
+you = "jworgnewsfeed@gmail.com"
+them = ["camster91@gmail.com"]
+# them = ["biancabashley@gmail.com","camster91@gmail.com",
+#         "kesjasamotik@hotmail.com","matthewplawn@gmail.com",
+#         "E.obszanska@hotmail.com","aaronm631@gmail.com",
+#         "nancyjashley@live.com","gordlashley@gmail.com",
+#         "Daniel-d-janeiro@live.ca", "chrislawrence4@hotmail.com",
+#         "mariah_papiah@hotmail.com", "aaroncastillo86@gmail.com",
+#         "janitacastillo85@gmail.com"
+#        ]
 
 msg = MIMEMultipart('alternative')
 msg['Subject'] = "New Posts From JW.ORG"
@@ -132,12 +191,12 @@ msg['To'] = you
 
 text = "HTML only. Please enable HTML email."
         
-html = open("C:/Python JW News/Start.html").read() + videos + news + "</table></body></html>"
+html = open("Start.html").read() + videos + news + books + "</table></body></html>"
 
 part1 = MIMEText(text, 'plain')
 part2 = MIMEText(html, 'html')
 
-if news_list or videos_list != []:
+if news_list or videos_list or book_list != []:
     msg.attach(part1)
     msg.attach(part2)
 
