fcc9ac730a0c8240d310d3a23554269ebaf71db8
Cameron <camster91@gmail.com>
2023-04-11 10:04:57 -0400

    2023

---
+++ JWDownload.py
@@ -0,0 +1,76 @@
+import time
+import os
+import os.path
+import requests
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+
+sections = ["https://www.jw.org/en/library/videos/#en/categories/BibleBooks"]
+
+for section in sections:
+
+    driver.get(section)
+
+    time.sleep(5)
+
+    html = driver.page_source
+    soup = BeautifulSoup(html, 'html.parser')
+
+    master_list = []
+
+    for link in soup.find_all("div", {"class":"syn-body lss"}):
+        title = link.text.strip()
+        master_list.append(title)
+
+        #if len(master_list) >= 1:
+            #break
+
+    for i in master_list:
+        
+        time.sleep(3)
+
+        nextButton = driver.find_element_by_link_text(i)
+        nextButton.click()
+
+        html = driver.page_source
+        soup = BeautifulSoup(html, 'html.parser')
+
+        time.sleep(3)
+
+        download_button = driver.find_element_by_class_name("dropdownHandle")
+        download_button.click()
+
+        html = driver.page_source
+        soup = BeautifulSoup(html, 'html.parser')
+
+        time.sleep(3)
+
+        file = soup.find('a', {'class':'secondaryButton'})
+        file_link = file["href"]
+        file_name = file["href"].split("/")[-1]
+
+        if  os.path.isfile(file_name):
+            driver.back()
+            continue
+
+        else:
+            r = requests.get(file_link, allow_redirects=True) 
+            open(file_name, 'wb').write(r.content)
+
+            time_to_wait = 120
+            time_counter = 0
+
+            while not os.path.exists(file_name):
+                time.sleep(1)
+                time_counter += 1
+                if time_counter > time_to_wait:break
+
+        driver.back()
+
+driver.quit()
--- jw_news_parser.py
+++ jw_news_parser.py
@@ -1,24 +1,24 @@
 import smtplib
-import pickle
 import feedparser
 import os
+import datetime
+import json
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.chrome.options import Options
 from bs4 import BeautifulSoup
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
-from subprocess import call
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 
-option = Options()
-option.headless = False
-driver = webdriver.Chrome(options=option)
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 
 os.environ['WDM_LOG_LEVEL'] = '0'
 
 driver.minimize_window()
+
 driver.get('https://www.jw.org/en/library/videos/#en/categories/LatestVideos')
 
 WebDriverWait(driver,100).until(EC.presence_of_element_located(
@@ -28,11 +28,14 @@ html = driver.page_source
 soup = BeautifulSoup(html, 'html.parser')
 
 videos_list = []
-old_links = []
+book_list = []
+pics_list = []
+news_list = []
+history = []
 
 try:
-    with open('C:\Python JW News\old_links.txt', 'rb') as filehandle:
-        old_links = pickle.load(filehandle)
+    with open('C:\Python JW News\history.json') as f:
+        history = json.load(f)
 except:
     print("File empty") 
                     
@@ -46,29 +49,24 @@ for link in soup.find_all("div", {"class":"synopsis lss desc showImgOverlay hasD
     links_dict['Image'] = image
     links_dict['Link'] = href
 
-    if href not in old_links:
+    if href not in history:
 
         videos_list.append(links_dict)
-        old_links.append(href)
+        history.append(href)
 
-        with open('C:\Python JW News\old_links.txt', 'wb') as filehandle:
-            pickle.dump(old_links, filehandle)
+        with open('C:\Python JW News\history.json', 'w', encoding='utf-8') as f:
+            json.dump(history, f, ensure_ascii=False, indent=4)
     else:
         continue
-
 
 driver.get('https://www.jw.org/en/library/books')
 
 WebDriverWait(driver,100).until(EC.presence_of_element_located(
         (By.ID, "pubsViewResults"))) 
 
-book_list = []
-old_books = []
-pics_list = []
-
 try:
-    with open('C:\Python JW News\old_books.txt', 'rb') as filehandle:
-        old_books = pickle.load(filehandle)
+    with open('C:\Python JW News\history.json') as f:
+        history = json.load(f)
 except:
     print("File empty") 
 
@@ -83,13 +81,13 @@ for link in  soup.find_all("div", {"class":"publicationDesc"}):
     books_dict['Link'] = href
     books_dict['Title'] = title
 
-    if href not in old_books:
+    if href not in history:
 
         book_list.append(books_dict)
-        old_books.append(href)
+        history.append(href)
 
-        with open('C:\Python JW News\old_books.txt', 'wb') as filehandle:
-                pickle.dump(old_books, filehandle)
+        with open('C:\Python JW News\history.json', 'w', encoding='utf-8') as f:
+            json.dump(history, f, ensure_ascii=False, indent=4)
     else:
         continue
 
@@ -100,12 +98,9 @@ for pic in soup.find_all("div", {"class":"cvr-wrapper"}):
 
 d = feedparser.parse('https://www.jw.org/en/whats-new/rss/WhatsNewWebArticles/feed.xml')
 
-news_list = []
-old_news = []
-
 try:
-    with open('C:\Python JW News\old_news.txt', 'rb') as filehandle:
-        old_news = pickle.load(filehandle)
+   with open('C:\Python JW News\history.json') as f:
+        history = json.load(f)
 except:
     print("File empty")
 
@@ -123,21 +118,46 @@ for entry in d.entries:
     except:
         news_dict['Image'] = ""
 
-    if news_link not in old_news:
+    if news_link not in history:
 
         news_list.append(news_dict)
-        old_news.append(news_link)
+        history.append(news_link)
 
-        with open('C:\Python JW News\old_news.txt', 'wb') as filehandle:
-            pickle.dump(old_news, filehandle)
+        with open('C:\Python JW News\history.json', 'w', encoding='utf-8') as f:
+            json.dump(history, f, ensure_ascii=False, indent=4)
     else:
         continue
 
-download_videos = False
 videos = ""
 news = ""
 books = ""
+reading = ""
+none = ""
 count = 0
+
+with open("C:\Python JW News\\bible.txt") as f:
+    bible = lines = f.readlines()
+
+with open("C:\Python JW News\\days.txt") as f:
+    reading_links = lines = f.readlines()
+
+x = datetime.datetime.now()
+
+day_of_year = x.strftime("%j")
+
+day_of_year = int(day_of_year)
+
+slice_of_year = day_of_year - 1
+
+day_list = bible[slice_of_year]
+
+script_list = reading_links[slice_of_year]
+
+today = x.strftime("%A, %B, %d")
+
+reading += open("C:\Python JW News\\reading.html").read().format(Script=script_list, Day=day_list, YearDay=day_of_year, Today=today)
+
+reading = "<tr><td class='content'><h1>Read the Bible Daily</h1>" + reading + "</td></tr>"
 
 for i in videos_list:
     videos += open("C:\Python JW News\\video.html").read().format(Text1=videos_list[count]['Title'], 
@@ -148,8 +168,7 @@ for i in videos_list:
 count = 0
 
 if videos_list != []:
-    videos = "<tr><td class='content'><h3>Latest Videos</h3>" + videos + "</td></tr>"
-    download_videos = True
+    videos = "<tr><td class='content'><h1>Latest Videos</h1>" + videos + "</td></tr>"
 
 for i in news_list:
     news += open("C:\Python JW News\\news.html").read().format(Text2=news_list[count]['Title'], 
@@ -160,7 +179,7 @@ for i in news_list:
 count = 0
 
 if news_list != []:
-    news = "<tr><td class='content'><h3>Latest News</h3>" + news + "</td></tr>"
+    news = "<tr><td class='content'><h1>Latest News</h1>" + news + "</td></tr>"
 
 for i in book_list:
     books += open("C:\Python JW News\\books.html").read().format(Text3=book_list[count]['Title'], 
@@ -168,51 +187,54 @@ for i in book_list:
                                             Img3=pics_list[count])
     count += 1
 
+count = 0
+
 if book_list != []:
-    books = "<tr><td class='content'><h3>Latest Books</h3>" + books + "</td></tr>"
+    books = "<tr><td class='content'><h1>Latest Books</h1>" + books + "</td></tr>"
+
+
 
 end = open("C:\Python JW News\end.html").read()
+
+emails = []
+email_list = []
+
+text = open("C:\Python JW News\email_list.txt", "r")
+emails = text.readlines()
+
+for line in emails:
+    email_list.append(line.strip())
     
-me = "JW Newsfeed"
+me = "JW Newsfeed <jworgnewsfeed@gmail.com>"
 you = "jworgnewsfeed@gmail.com"
-#them = ["camster91@gmail.com"]
-them = ["biancabashley@gmail.com","camster91@gmail.com",
-        "kesjasamotik@hotmail.com","matthewplawn@gmail.com",
-        "E.obszanska@hotmail.com","aaronm631@gmail.com",
-        "nancyjashley@live.com","gordlashley@gmail.com",
-        "Daniel-d-janeiro@live.ca", "chrislawrence4@hotmail.com",
-        "mariah_papiah@hotmail.com", "aaroncastillo86@gmail.com",
-        "janitacastillo85@gmail.com", "Bernard_Morissette@hotmail.com",
-        "kayavery@comcast.net"
-       ]
+them = ["camster91@gmail.com"]
+#them = email_list
 
 msg = MIMEMultipart('alternative')
-msg['Subject'] = "New Posts From JW.ORG"
+msg['Subject'] = "JW.ORG Update"
 msg['From'] = me
 msg['To'] = you
 
 text = "HTML only. Please enable HTML email."
         
-html = open("C:\Python JW News\main.html").read() + videos + news + books + end
+html = open("C:\Python JW News\main.html").read() + reading + videos + news + books + none + end
 
 part1 = MIMEText(text, 'plain')
-
 part2 = MIMEText(html, 'html')
 
-#print(html)
+#if news_list or videos_list or book_list != []:
+    
+msg.attach(part1)
+msg.attach(part2)
 
-if news_list or videos_list or book_list != []:
-    msg.attach(part1)
-    msg.attach(part2)
+mail = smtplib.SMTP('smtp-relay.sendinblue.com:587')
 
-    mail = smtplib.SMTP('smtp.gmail.com', 587)
+#mail.ehlo()
 
-    mail.ehlo()
+mail.starttls()
 
-    mail.starttls()
-
-    mail.login('jworgnewsfeed@gmail.com', 'Willow123!')
-    mail.sendmail(me, [you] + them, msg.as_string())
-    mail.quit()
+mail.login('camster91@gmail.com', '0h65mJPSk7Iz4Dwn')
+mail.sendmail(me, [you] + them, msg.as_string())
+mail.quit()
 
 driver.quit()
